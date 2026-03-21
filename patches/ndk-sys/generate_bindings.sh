4a054d5198a5deaba2dbb19b6c4c2105cf6315c7
phiresky <phireskyde+git@gmail.com>
2026-03-21 23:53:38 +0100

    ndk-sys

---
+++ patches/ndk-sys/generate_bindings.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+set -ex
+
+# Use local NDK r29 sysroot instead of downloading from CI
+sysroot="$HOME/tmp/android-sdk/ndk/29.0.14206865/toolchains/llvm/prebuilt/linux-x86_64/sysroot/"
+[ ! -d "$sysroot" ] && echo "Android sysroot $sysroot does not exist!" && exit 1
+
+while read ARCH && read TARGET ; do
+    bindgen wrapper.h -o src/ffi_$ARCH.rs \
+        --rust-target 1.60 \
+        --blocklist-item 'JNI\w+' \
+        --blocklist-item 'C?_?JNIEnv' \
+        --blocklist-item '_?JavaVM' \
+        --blocklist-item '_?j\w+' \
+        --newtype-enum '\w+_(result|status)_t' \
+        --newtype-enum 'ACameraDevice_request_template' \
+        --newtype-enum 'ADataSpace' \
+        --newtype-enum 'AHardwareBuffer_Format' \
+        --newtype-enum 'AHardwareBuffer_UsageFlags' \
+        --newtype-enum 'AHdrMetadataType' \
+        --newtype-enum 'AIMAGE_FORMATS' \
+        --newtype-enum 'AMediaDrmEventType' \
+        --newtype-enum 'AMediaDrmKeyRequestType' \
+        --newtype-enum 'AMediaDrmKeyType' \
+        --newtype-enum 'AMediaKeyStatusType' \
+        --newtype-enum 'AMidiDevice_Protocol' \
+        --newtype-enum 'AMotionClassification' \
+        --newtype-enum 'ANativeWindowTransform' \
+        --newtype-enum 'ANativeWindow_ChangeFrameRateStrategy' \
+        --newtype-enum 'ANativeWindow_FrameRateCompatibility' \
+        --newtype-enum 'ANativeWindow_LegacyFormat' \
+        --newtype-enum 'ASurfaceTransactionTransparency' \
+        --newtype-enum 'ASurfaceTransactionVisibility' \
+        --newtype-enum 'AndroidBitmapCompressFormat' \
+        --newtype-enum 'AndroidBitmapFormat' \
+        --newtype-enum 'AppendMode' \
+        --newtype-enum 'DeviceTypeCode' \
+        --newtype-enum 'DurationCode' \
+        --newtype-enum 'FeatureLevelCode' \
+        --newtype-enum 'FuseCode' \
+        --newtype-enum 'HeapTaggingLevel' \
+        --newtype-enum 'OperandCode' \
+        --newtype-enum 'OperationCode' \
+        --newtype-enum 'OutputFormat' \
+        --newtype-enum 'PaddingCode' \
+        --newtype-enum 'PreferenceCode' \
+        --newtype-enum 'PriorityCode' \
+        --newtype-enum 'ResNsendFlags' \
+        --newtype-enum 'ResultCode' \
+        --newtype-enum 'SeekMode' \
+        --newtype-enum 'acamera_\w+' \
+        --newtype-enum 'android_LogPriority' \
+        --newtype-enum 'android_fdsan_error_level' \
+        --newtype-enum 'android_fdsan_owner_type' \
+        --newtype-enum 'cryptoinfo_mode_t' \
+        --newtype-enum 'log_id' \
+        -- \
+        --sysroot="$sysroot" --target=$TARGET
+done << EOF
+arm
+arm-linux-androideabi
+aarch64
+aarch64-linux-android
+i686
+i686-linux-android
+x86_64
+x86_64-linux-android
+EOF
